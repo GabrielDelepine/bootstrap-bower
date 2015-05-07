5a75665222252e75b2fae2d10a50246c88ecc2d7
Gabriel Delépine <gabriel.delepine@qapa.com>
2015-05-07 17:38:11 +0200

    0.12.1.3 fix 0.12.1.2

--- ui-bootstrap-tpls.js
+++ ui-bootstrap-tpls.js
@@ -3908,7 +3908,7 @@ angular.module("template/accordion/accordion-group.html", []).run(["$templateCac
     "      <a href=\"javascript:void(0)\" class=\"accordion-toggle\" ng-click=\"toggleOpen()\" accordion-transclude=\"heading\"><span ng-class=\"{'text-muted': isDisabled}\">{{heading}}</span></a>\n" +
     "    </h4>\n" +
     "  </div>\n" +
-    "  <div class=\"panel-collapse\" collapse=\"!isOpen\">\n" +
+    "  <div class=\"panel-collapse\" ng-hide=\"!isOpen\">\n" +
     "	  <div class=\"panel-body\" ng-transclude></div>\n" +
     "  </div>\n" +
     "</div>\n" +
